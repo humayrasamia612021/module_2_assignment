31ad92eca7c61f68c4f56efc347affaf21af8f87
humayrasamia612021 <samia.salam0612@gmail.com>
2026-09-03 05:23:53 +0600

    Add loops and improve output

--- bin/smart_temperature_converter.dart
+++ bin/smart_temperature_converter.dart
@@ -3,97 +3,106 @@ import 'dart:io';
 void main() {
   bool running = true;
 
-  print('====================================');
-  print('       SMART TEMPERATURE CONVERTER');
-  print('====================================');
-  print('1. Celsius to Fahrenheit');
-  print('2. Fahrenheit to Celsius');
-  print('3. Celsius to Kelvin');
-  print('4. Kelvin to Celsius');
-  print('5. Fahrenheit to Kelvin');
-  print('6. Kelvin to Fahrenheit');
-  print('7. Exit');
+  while (running) {
 
-  stdout.write('Enter your choice: ');
-  String? choice = stdin.readLineSync();
+    print('====================================');
+    print('       SMART TEMPERATURE CONVERTER');
+    print('====================================');
+    print('1. Celsius to Fahrenheit');
+    print('2. Fahrenheit to Celsius');
+    print('3. Celsius to Kelvin');
+    print('4. Kelvin to Celsius');
+    print('5. Fahrenheit to Kelvin');
+    print('6. Kelvin to Fahrenheit');
+    print('7. Exit');
 
-  switch (choice){
-    case '1':
-      stdout.write('Enter temperature in Celsius: ');
-      double celsius = double.parse(stdin.readLineSync()!);
+    stdout.write('Enter your choice: ');
+    String? choice = stdin.readLineSync();
 
-      double fahrenheit = (celsius * 9 / 5) + 32;
+    switch (choice){
+      case '1':
+        stdout.write('Enter temperature in Celsius: ');
+        double celsius = double.parse(stdin.readLineSync()!);
 
-      print('$celsius°C = $fahrenheit°F');
-      break;
+        double fahrenheit = (celsius * 9 / 5) + 32;
 
-    case '2':
-      stdout.write('Enter temperature in Fahrenheit: ');
-      double fahrenheit = double.parse(stdin.readLineSync()!);
+        print('$celsius°C = $fahrenheit°F');
+        break;
 
-      double celsius = (fahrenheit - 32) * 5 / 9;
+      case '2':
+        stdout.write('Enter temperature in Fahrenheit: ');
+        double fahrenheit = double.parse(stdin.readLineSync()!);
 
-      print('$fahrenheit°F = $celsius°C');
-      break;
+        double celsius = (fahrenheit - 32) * 5 / 9;
 
-    case '3':
-      stdout.write('Enter temperature in Celsius: ');
-      double celsius = double.parse(stdin.readLineSync()!);
+        print('$fahrenheit°F = $celsius°C');
+        break;
 
-      double kelvin = celsius + 273.15;
+      case '3':
+        stdout.write('Enter temperature in Celsius: ');
+        double celsius = double.parse(stdin.readLineSync()!);
 
-      print('$celsius°C = $kelvin K');
-      break;
+        double kelvin = celsius + 273.15;
 
-    case '4':
-      stdout.write('Enter temperature in Kelvin: ');
-      double kelvin = double.parse(stdin.readLineSync()!);
+        print('$celsius°C = $kelvin K');
+        break;
 
-      double celsius = kelvin - 273.15;
+      case '4':
+        stdout.write('Enter temperature in Kelvin: ');
+        double kelvin = double.parse(stdin.readLineSync()!);
 
-      print('$kelvin K = $celsius°C');
-      break;
+        double celsius = kelvin - 273.15;
 
-    case '5':
-      stdout.write('Enter temperature in Fahrenheit: ');
-      double fahrenheit = double.parse(stdin.readLineSync()!);
+        print('$kelvin K = $celsius°C');
+        break;
 
-      double kelvin = (fahrenheit - 32) * 5 / 9 + 273.15;
+      case '5':
+        stdout.write('Enter temperature in Fahrenheit: ');
+        double fahrenheit = double.parse(stdin.readLineSync()!);
 
-      print('$fahrenheit°F = $kelvin K');
-      break;
+        double kelvin = (fahrenheit - 32) * 5 / 9 + 273.15;
 
-    case '6':
-      stdout.write('Enter temperature in Kelvin: ');
-      double kelvin = double.parse(stdin.readLineSync()!);
+        print('$fahrenheit°F = $kelvin K');
+        break;
 
-      double fahrenheit = (kelvin - 273.15) * 9 / 5 + 32;
+      case '6':
+        stdout.write('Enter temperature in Kelvin: ');
+        double kelvin = double.parse(stdin.readLineSync()!);
 
-      print('$kelvin K = $fahrenheit°F');
-      break;
+        double fahrenheit = (kelvin - 273.15) * 9 / 5 + 32;
 
-    case '7':
-      running = false;
-      break;
+        print('$kelvin K = $fahrenheit°F');
+        break;
 
-    default:
-      print('Invalid choice. Please enter a number from 1 to 7.');
-      continue;
+      case '7':
+        running = false;
+        break;
 
-  }
+      default:
+        print('Invalid choice. Please enter a number from 1 to 7.');
+        continue;
 
-  if (running) {
-    stdout.write('Do you want to perform another conversion? (y/n): ');
-    String? answer = stdin.readLineSync();
-
-    if (answer == 'y') {
-      continue;
-    } else if (answer == 'n') {
-      running = false;
-    } else {
-      print('Invalid input. Please enter y or n.');
-      running = false;
     }
+
+    if (running) {
+      stdout.write('Do you want to perform another conversion? (y/n): ');
+      String? answer = stdin.readLineSync();
+
+      if (answer == 'y') {
+        continue;
+      } else if (answer == 'n') {
+        running = false;
+      } else {
+        print('Invalid input. Please enter y or n.');
+        running = false;
+      }
+    }
+
   }
+
+  print('====================================');
+  print('          Thank You!');
+  print('          Program Ended');
+  print('====================================');
 
 }
